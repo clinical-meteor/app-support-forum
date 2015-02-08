62b212de8b282160a84c98354cd43b5f2d9e9ebf
Abigail Watson <awatson1978@gmail.com>
2015-02-08 13:11:54 -0500

    updated mainLayout and navbars; passing tests

--- webapp/tests/nightwatch/walkthrough.js
+++ webapp/tests/nightwatch/walkthrough.js
@@ -1,6 +1,6 @@
 // tests/leaderboard.js
 module.exports = {
-  "ActiveRecord::Table Walkthrough" : function (client) {
+  "Basic Walkthrough" : function (client) {
     client
     .url("http://localhost:3000")
     .waitForElementVisible('body', 1000)
